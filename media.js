8a0f3e34312f89a8e0df7b2a9c55302a7104dce9
Melissa Victória <melzinhalacerdaaraujo@gmail.com>
2022-08-08 11:10:49 -0300

    sistema para calculo de media

--- media.js
+++ media.js
@@ -7,6 +7,8 @@
 
 console.log('Sistema para Cálculo de média')
 
+const {calcular} = require('./modulos/sistmedia')
+
 var readline = require('readline');
 
 var entradaDados = readline.createInterface({
@@ -16,46 +18,58 @@ var entradaDados = readline.createInterface({
 
 entradaDados.question('Digite o nome do aluno: \n ' , function (nome) {
     let nomeAluno = nome; 
-
-});
-
-entradaDados.question('Digite o nome do professor: \n' , function(professor) {
-    let nomeProfessor = professor;
-});
-
-entradaDados.question('Informe o gênero do aluno: \n ', function(genero1) {
-    let generoAluno = genero1;
-});
-
-entradaDados.question('Informe o gênero do professor: \n ', function(genero2) {
-    let generoProfessor = genero2; 
-});
-
-entradaDados.question('Digite o nome do curso: \n', function(curso) {
-    let nomeCurso = curso;
-});
-
-entradaDados.question('Digite o nome da disciplina: \n' , function(disciplina){
-    let nomeDisciplina = disciplina;
-});
-
-entradaDados.question('Insira o valor da nota 1: \n ', function(valor1){
-    let nota1 = valor1;
-});
-
-entradaDados.question('Insira o valor da nota 2: \n', function(valor2) {
-    let nota2 = valor2;
-});
-
-entradaDados.question('Insira o valor da nota 3: \n', function(valor3){
-    let nota3 = valor3;
-});
-
-entradaDados.question('Insira o valor da nota 4: \n' , function(valor4){
-    let nota4 = valor4;
+    entradaDados.question('Digite o nome do professor: \n' , function(professor) {
+        let nomeProfessor = professor;
+        entradaDados.question('Informe o gênero do aluno: \n ', function(genero1) {
+            let generoAluno = genero1;
+            entradaDados.question('Digite o nome do curso: \n', function(curso) {
+                let nomeCurso = curso;
+                entradaDados.question('Digite o nome da disciplina: \n' , function(disciplina){
+                    let nomeDisciplina = disciplina;
+                    entradaDados.question('Insira o valor da nota 1: \n ', function(valor1){
+                        let nota1 = valor1;
+                        entradaDados.question('Insira o valor da nota 2: \n', function(valor2) {
+                            let nota2 = valor2;
+                            entradaDados.question('Insira o valor da nota 3: \n', function(valor3){
+                                let nota3 = valor3;
+                                entradaDados.question('Insira o valor da nota 4: \n' , function(valor4){
+                                    let nota4 = valor4;
+                                    calcular(nota1, nota2, nota3, nota4)
+                                     
+                                   
+                                });
+                            });
+                        });
+                    });
+                });
+            
+            
+            })
+        
+        });
+    });
 });
 
 
-  if (nomeAluno == '' ) {
-    console.log('O nome do aluno deve ser informado')
-}
+
+
+
+
+
+;
+
+
+
+
+
+
+
+
+
+
+ 
+
+//if(valor1== ''|| valor2 == '' || valor3 == '' || valor4 || nome || professor || genero1 || genero2 || curso || disciplina ) {
+  //  console.log('Para continuar, você deve inserir todos os dados.')
+//}
+
--- modulos/sistmedia.js
+++ modulos/sistmedia.js
@@ -5,22 +5,41 @@
  * Versão: 1.0
  */
 
+// const { exit } = require("process");
+
 console.log('Sistema para Cálculo de Média ')
 
-function entrada (nome, professor, genero1, genero2, curso, disciplina, valor1, valor2, valor3, valor4) {
-
-let 
 
 
+//Funçao calcular média.
+function calcular (nota1, nota2, nota3, nota4) {
+
+    let media;
+    let statusAluno;
+    
+  // if(nota1 == '' || nota2 == '' || nota3 == '' || nota4 == '' ) {
+    //console.log('Para calcular a média todas as notas precisam ser informadas.')}  (
+   media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4)) /4;
+    console.log(console.log('O aluno['+nomeAluno + '], teve media'+ media)); 
 
 
-}
+    if(nota1 <=100 || nota2 <=100|| nota3 <=100 || nota4 <=100) {
 
+    } else {
+        console.log('As notas devem respeitar o intervalo de 0 a 100.')
+    }
 
+    if(media => 70) {
+     statusAluno = APROVADO;
+    } else if (media >= 50 && media <= 69 )
+    statusAluno = EXAME;
+    else (media >=49)
+        statusAluno = REPROVADO;
+    }
 
 
 
 
 module.exports = {
-    entrada
+ calcular
 }
