930f0efbea3f9f74c6ac122aeec4be3fcfbc4ab5
Melissa Victória <melzinhalacerdaaraujo@gmail.com>
2022-08-11 08:34:33 -0300

    atualizações

--- modulos/sistmedia.js
+++ modulos/sistmedia.js
@@ -5,20 +5,17 @@
  * Versão: 1.0
  */
 
-// const { exit } = require("process");
-
 console.log('Sistema para Cálculo de Média ')
 
-
-
 //Funçao calcular média.
-function calcular (nota1, nota2, nota3, nota4) {
+function calcular (nota1, nota2, nota3, nota4, statusAluno, notaExame ) {
 
     let media;
-    let statusAluno;
-    
-  // if(nota1 == '' || nota2 == '' || nota3 == '' || nota4 == '' ) {
-    //console.log('Para calcular a média todas as notas precisam ser informadas.')}  (
+      
+  if(nota1 == '' || nota2 == '' || nota3 == '' || nota4 == '' ) {
+    console.log('Para calcular a média todas as notas precisam ser informadas.')} 
+
+
    media = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4)) /4;
     console.log(console.log('O aluno['+nomeAluno + '], teve media'+ media)); 
 
@@ -29,17 +26,33 @@ function calcular (nota1, nota2, nota3, nota4) {
         console.log('As notas devem respeitar o intervalo de 0 a 100.')
     }
 
-    if(media => 70) {
-     statusAluno = APROVADO;
-    } else if (media >= 50 && media <= 69 )
-    statusAluno = EXAME;
-    else (media >=49)
-        statusAluno = REPROVADO;
+
+    //função para validar as notas e a situação do aluno. 
+    const exame = function (notaExame, statusAluno) {
+
+        let notaExame;
+        let statusAluno;
+
+        if(media >= 70) {
+            statusAluno = APROVADO;
+           } else if (media >= 50 && media <= 69 )
+           statusAluno = EXAME;
+           else (media >=49)
+               statusAluno = REPROVADO;
+           }
+       
+           if(statusAluno == EXAME ) {
+               console.log('Insira a nota de exame do aluno:')
+           }
+           else if (notaExame >= 60) {
+               console.log ('O aluno foi APROVADO')
+           } 
+
     }
 
 
-
+   
 
 module.exports = {
- calcular
+ calcular , exame  
 }
